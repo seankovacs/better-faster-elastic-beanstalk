74a0d2a36fef9d4004882385e0fdb2a165edec6c
Sean Kovacs <seankovacs@Seans-MBP.fios-router.home>
2016-05-20 21:16:43 -0400

    Updates

--- 40install_node.sh
+++ 40install_node.sh
@@ -14,7 +14,7 @@ exec >>/var/log/cfn-init.log  2>&1
 echo "checking node..."
 OUT=$( [ ! -d "/opt/elasticbeanstalk/node-install" ] && echo "trying to install node.js $NODE_VER"   && mkdir /opt/elasticbeanstalk/node-install ; cd /opt/elasticbeanstalk/node-install/ && \
   wget -nc http://nodejs.org/dist/v$NODE_VER/node-v$NODE_VER-linux-$ARCH.tar.gz && \
-  tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz) || error_exit "Failed to UPDATE node version. $OUT" $?.
+  tar --skip-old-files -xzpf node-v$NODE_VER-linux-$ARCH.tar.gz && node -v) || error_exit "Failed to UPDATE node version. $OUT" $?.
 echo $OUT
 
 
